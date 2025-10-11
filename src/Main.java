ee6f0d9b737528385ab1f55d6baba8e882c38238
Lilwrx201206 <24022641@vnu.edu.vn>
2025-10-11 12:53:31 +0700

    frame

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
-
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello World");
+import javax.swing.*;
+public class Main extends JFrame {
+    public Main(){
+        setTitle("Arkanoid Game");
+        setSize(800,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::new);
     }
 }
