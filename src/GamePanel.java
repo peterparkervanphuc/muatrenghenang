55985def9655145fa10e70c91d44b048ff0c27a7
Lilwrx201206 <24022641@vnu.edu.vn>
2025-10-11 13:08:28 +0700

    ball

---
+++ src/GamePanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+public class GamePanel extends JPanel {
+    public GamePanel() {
+        setPreferredSize(new Dimension(800, 600));
+        setBackground(Color.BLACK);
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.drawString("Arkanoid go",350,300);
+    }
+}
