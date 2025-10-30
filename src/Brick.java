1a3c3cfca6fdabce5e17f3aa0a933582ec81f975
TBinh24022617 <24022617@vnu.edu.vn>
2025-10-30 09:14:09 +0700

    Xây dựng hệ thống Power-up và thêm Gạch Lửa (Fireball) và laser,mạng sống

--- src/Brick.java
+++ src/Brick.java
@@ -6,19 +6,19 @@ public class Brick extends GameObject {
     private static final int BRICK_WIDTH = 70;
     private static final int BRICK_HEIGHT = 20;
     private boolean isVisible;
+    private BrickType type;  // Replace isUnbreakable with type
 
-    public Brick(int x, int y) {
+    public Brick(int x, int y, BrickType type) {
         super(x, y, BRICK_WIDTH, BRICK_HEIGHT);
         this.isVisible = true;
+        this.type = type;
     }
 // GHI đè (Override) phương thức 'draw' từ GameObject để vẽ viên gạch.
     @Override
     public void draw(Graphics g) {
         if (isVisible) {
-            g.setColor(Color.ORANGE);
+            g.setColor(type.getColor());
             g.fillRect(x, y, width, height);
-
-            // Adding a border to the brick for better visibility
             g.setColor(Color.BLACK);
             g.drawRect(x, y, width, height);
         }
@@ -31,5 +31,9 @@ public class Brick extends GameObject {
     public void setVisible(boolean isVisible) {
         this.isVisible = isVisible;
     }
-}
 
+    // Remove isUnbreakable() method
+    public BrickType getType() {
+        return type;
+    }
+}
